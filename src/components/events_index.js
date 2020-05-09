b53ea3e3915dd08ac6558dee391ff473916dcae5
ckr344 <ckr344.225@gmail.com>
2020-05-10 00:39:02 +0900

    Inplement events index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,8 +1,52 @@
-// import React from 'react';
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
-import { increment, decrement } from '../actions'
+import _ from 'lodash'
+
+import { readEvents } from '../actions'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body }</td>
+      </tr>
+    ))
+  }
+
+  render(){
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const mapStateToProps = state => ({ events: state.events })
+
+const mapDispatchToProps = ({ readEvents })
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
 
 // function App() {
 //   // const greeting = "Hi!!";
@@ -53,40 +97,20 @@ import { increment, decrement } from '../actions'
 
 // const App = () => ( <Counter> </Counter> )
 
-class EventsIndex extends Component {
-  // Reduxを使用する場合は同じ処理をreducerで行うことになるため記述が不要となる
-  // constructorは初期化処理で実行されるメソッド(コールバック)
-  // constructor(props){
-  //   super(props)
-  //   this.state = {count: 0}
-  // }
+// Reduxを使用する場合は同じ処理をreducerで行うことになるため記述が不要となる
+// constructorは初期化処理で実行されるメソッド(コールバック)
+// constructor(props){
+//   super(props)
+//   this.state = {count: 0}
+// }
 
-  // Reduxを使用する場合は同じ処理をaction creatorで実行しているため不要となる
-  // handlePlusButton = () => {
-  //   this.setState({count: this.state.count + 1})
-  // }
-  // handleMinusButton = () => {
-  //   this.setState({count: this.state.count - 1})
-  // }
-
-  render(){
-    const props = this.props
-
-    return(
-      <React.Fragment>
-        <div>value: { props.value }</div>
-        <button onClick={ props.increment }>+1</button>
-        <button onClick={ props.decrement }>-1</button>
-      </React.Fragment>
-    )
-  }
-}
-
-const mapStateToProps = state => ({ value: state.count.value })
-const mapDispatchToProps = dispatch => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement())
-})
+// Reduxを使用する場合は同じ処理をaction creatorで実行しているため不要となる
+// handlePlusButton = () => {
+//   this.setState({count: this.state.count + 1})
+// }
+// handleMinusButton = () => {
+//   this.setState({count: this.state.count - 1})
+// }
 
 // const User = (props) => {
 //   return <div>Hi! I am {props.name}!, and {props.age} years old! </div>
@@ -105,7 +129,5 @@ const mapDispatchToProps = dispatch => ({
 // User.defaultProps = {
 //   age: 1
 // }
-
-export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
 
 // export default App;
---
+++ src/reducers/events.js
@@ -0,0 +1,14 @@
+// import { INCREMENT, DECREMENT } from '../actions'
+import _ from 'lodash'
+import { READ_EVENTS } from '../actions'
+
+// const initialState = { value: 0 }
+
+export default (events = {}, action) => {
+  switch (action.type) {
+    case READ_EVENTS:
+      return _.mapKeys(action.response.data, 'id')
+    default:
+      return events
+  }
+}
